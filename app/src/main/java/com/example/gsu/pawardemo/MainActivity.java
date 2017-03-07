1ad72322fe1fcba6be19dff1a8995eb16d81e46d
GSU <spawar2@student.gsu.edu>
2017-03-06 19:16:32 -0500

    Commit 02/01

--- app/src/main/java/com/example/gsu/pawardemo/MainActivity.java
+++ app/src/main/java/com/example/gsu/pawardemo/MainActivity.java
@@ -2,27 +2,40 @@ package com.example.gsu.pawardemo;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.ScaleGestureDetectorCompat;
 import android.support.v4.view.ViewPager;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnTouchListener;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.gsu.pawardemo.bean.Book;
 import com.example.gsu.pawardemo.util.UtilLog;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnCheckedChanged;
 import butterknife.OnClick;
 
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements View.OnTouchListener{
 
     private ImageButton bt1;
     private ImageButton bt3;
     private ImageButton activityswitch;
     private Button main_timer_bt;
     private Button main_timer_bt_2;
+    private Button main_timer_bt_3;
+    private GestureDetector mGestureDetector;
+
+    @BindView(R.id.main_fl)
+    RelativeLayout fl;
 
     @OnClick(R.id.bt2)
     public void button2Click(){
@@ -37,6 +50,8 @@ public class MainActivity extends BaseActivity {
         initialView();
         initialListener();
         ButterKnife.bind(this);
+        mGestureDetector = new GestureDetector(this, new simpleGestureListener());
+        fl.setOnTouchListener(this);
 
     }
 
@@ -46,6 +61,7 @@ public class MainActivity extends BaseActivity {
         activityswitch = (ImageButton) findViewById(R.id.activityswitch);
         main_timer_bt = (Button) findViewById(R.id.main_timer_bt);
         main_timer_bt_2 = (Button) findViewById(R.id.main_timer_bt_2);
+        main_timer_bt_3 = (Button) findViewById(R.id.main_timer_bt_3);
     }
 
     private void initialListener() {
@@ -107,6 +123,19 @@ public class MainActivity extends BaseActivity {
             }
         });
 
+        main_timer_bt_3.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+
+                //       Intent intent = new Intent(v.getContext(), ListViewActivity.class);
+                //     startActivity(intent);
+                Intent intent = new Intent(v.getContext(), AnimatorActivity.class);
+                startActivityForResult(intent,3);
+            }
+        });
+
         activityswitch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -148,5 +177,63 @@ public class MainActivity extends BaseActivity {
 
     }
 
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        return mGestureDetector.onTouchEvent(event);
+    }
+
+    private class simpleGestureListener extends GestureDetector.SimpleOnGestureListener{
+        public boolean onDown(MotionEvent e) {
+            UtilLog.logD("My Gesture","onDown");
+            toastShort( "onDown");
+            return false;
+        }
+
+        public void onShowPress(MotionEvent e){
+            UtilLog.logD("My Gesture", "onShowPress");
+            toastShort("onShowPress");
+        }
+
+        public void onLongPress(MotionEvent e){
+            UtilLog.logD("My Gesture", "onLongPress");
+            toastShort("onLongPress");
+        }
+
+        public boolean onSingleTapUp(MotionEvent e){
+            UtilLog.logD("My Gesture", "onSingleTapUp");
+            toastShort("onSingleTapUp");
+            return true;
+        }
+
+        public boolean onSingleTapConfirmed(MotionEvent e){
+
+            toastShort("onSingleTapConfirmed");
+            return true;
+        }
+
+        public boolean onScroll (MotionEvent e1, MotionEvent e2, float distanceX , float distanceY) {
+            UtilLog.logD("MyGesture", "onScroll:" + (e2.getX() - e1.getX()) + " " + distanceX);
+            toastShort("onScroll");
+            return true;
+        }
+
+        public boolean onFling (MotionEvent e1, MotionEvent e2, float velocityX , float velocityY) {
+            toastShort("onFling");
+            return true;
+        }
+
+
+        public boolean onDoubleTap (MotionEvent e) {
+            toastShort("onDoubleTap");
+            return true;
+        }
+
+        public boolean onDoubleTapEvent (MotionEvent e) {
+            toastShort("onDoubleTapEvent");
+            return true;
+        }
+
+    }
 
 }
